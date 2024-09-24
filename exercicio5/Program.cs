a39556052fb38427fe110652c5f40fdba8c186e9
GabrielFilipe23 <gabrielcondee02@gmai.com>
2024-09-24 13:56:52 -0300

    exercicio5

---
+++ exercicio5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string frase = "Olá mundo!"; 
+        Console.WriteLine("String original: " + frase);
+
+        char[] caracteres = new char[frase.Length];
+
+        for (int i = 0; i < frase.Length; i++)
+        {
+            caracteres[i] = frase[frase.Length - 1 - i];
+        }
+
+        string stringInvertida = new string(caracteres);
+        Console.WriteLine("String invertida: " + stringInvertida);
+    }
+}
